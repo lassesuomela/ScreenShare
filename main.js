780e6ed9e1de884c9fcbb08f97d99f80441094af
Lasse Suomela <suomelasse@gmail.com>
2023-04-29 22:21:26 +0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,49 @@
+const { app, BrowserWindow, ipcMain, desktopCapturer } = require("electron");
+const path = require("path");
+
+const remoteMain = require("@electron/remote/main");
+remoteMain.initialize();
+
+if (require("electron-squirrel-startup")) {
+  // eslint-disable-line global-require
+  app.quit();
+}
+
+try {
+  require("electron-reloader")(module);
+} catch (_) {}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  remoteMain.enable(mainWindow.webContents);
+
+  mainWindow.loadFile(path.join(__dirname, "index.html"));
+
+  mainWindow.webContents.openDevTools();
+};
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
+
+ipcMain.handle("getSources", async () => {
+  return await desktopCapturer.getSources({ types: ["window", "screen"] });
+});
---
+++ render.js
@@ -0,0 +1,76 @@
+const { desktopCapturer, ipcRenderer } = require("electron");
+const remote = require("@electron/remote");
+
+const { Menu } = remote;
+
+let videoOptionsMenu;
+
+const videoElement = document.querySelector("video");
+
+const startBtn = document.getElementById("startBtn");
+const stopBtn = document.getElementById("stopBtn");
+const videoSelectBtn = document.getElementById("videoSelectBtn");
+const currentSourceText = document.getElementById("currentSourceText");
+
+startBtn.onclick = (e) => {
+  if (videoElement.srcObject === null) {
+    return;
+  }
+
+  startBtn.classList.add("is-danger");
+  startBtn.innerText = "Streaming";
+  startBtn.setAttribute("disabled", "true");
+};
+
+stopBtn.onclick = (e) => {
+  if (videoElement.srcObject === null) {
+    return;
+  }
+
+  startBtn.classList.remove("is-danger");
+  startBtn.innerText = "Start";
+  startBtn.removeAttribute("disabled");
+
+  videoElement.srcObject = null;
+  currentSourceText.innerText = "-";
+};
+
+videoSelectBtn.onclick = showSources;
+
+function showSources() {
+  videoOptionsMenu.popup();
+}
+
+async function getSources() {
+  const inputSources = await ipcRenderer.invoke("getSources");
+
+  videoOptionsMenu = Menu.buildFromTemplate(
+    inputSources.map((source) => {
+      return {
+        label: source.name,
+        click: () => selectSource(source),
+      };
+    })
+  );
+}
+
+async function selectSource(source) {
+  currentSourceText.innerText = source.name;
+
+  const constraints = {
+    audio: false,
+    video: {
+      mandatory: {
+        chromeMediaSource: "desktop",
+        chromeMediaSourceId: source.id,
+      },
+    },
+  };
+
+  const stream = await navigator.mediaDevices.getUserMedia(constraints);
+
+  videoElement.srcObject = stream;
+  videoElement.play();
+}
+
+getSources();
